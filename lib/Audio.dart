e521ca4ac151315119a23308a709c0ca3b7c4322
MD DILKHUSH <140965939+dilkhush2001@users.noreply.github.com>
2024-07-08 17:16:49 +0530

    Update Audio.dart
    Added Payment Gateway

--- lib/Audio.dart
+++ lib/Audio.dart
@@ -1,7 +1,10 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_paypal_checkout/flutter_paypal_checkout.dart';
 import 'package:provider/provider.dart';
+import 'Login_User/PaymentGatway.dart';
+import 'Payment Gateway/home_screen.dart';
 import 'main.dart';
 import 'music.dart';
 import 'musicScreen.dart';
@@ -15,9 +18,73 @@ class AudioListScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
         backgroundColor: Colors.blue,
         title: Text('Audio List'),
+        actions: [
+          ElevatedButton(onPressed: (){Navigator.of(context).push(MaterialPageRoute(
+            builder: (BuildContext context) =>
+                PaypalCheckout(
+                  sandboxMode: true,
+                  clientId: "u can give ur or else i will send u personal",
+                  secretKey: "SAme .......",
+                  returnURL: "www.google.com",
+                  cancelURL: "www.google.com",
+                  transactions: const [
+                    {
+                      "amount": {
+                        "total": '70',
+                        "currency": "USD",
+                        "details": {
+                          "subtotal": '70',
+                          "shipping": '0',
+                          "shipping_discount": 0
+                        }
+                      },
+                      "description": "The payment transaction description.",
+                      "item_list": {
+                        "items": [
+                          {
+                            "name": "Apple",
+                            "quantity": 4,
+                            "price": '5',
+                            "currency": "USD"
+                          },
+                          {
+                            "name": "Pineapple",
+                            "quantity": 5,
+                            "price": '10',
+                            "currency": "USD"
+                          }
+                        ],
+                        // shipping address is Optional
+                        "shipping_address": {
+                          "recipient_name": "Raman Singh",
+                          "line1": "Delhi",
+                          "line2": "",
+                          "city": "Delhi",
+                          "country_code": "IN",
+                          "postal_code": "11001",
+                          "phone": "+00000000",
+                          "state": "Texas"
+                        },
+                      }
+                    }
+                  ],
+                  note: "PAYMENT_NOTE",
+                  onSuccess: (Map params) async {
+                    print("onSuccess: $params");
+                  },
+                  onError: (error) {
+                    print("onError: $error");
+                    Navigator.pop(context);
+                  },
+                  onCancel: () {
+                    print('cancelled:');
+                  },
+                ),
+
+          ));}, child: Text('Subsciption', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),))
+        ],
       ),
       body: Column(
         children: [
